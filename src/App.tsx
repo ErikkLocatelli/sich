d2960ddd59551c5afbd87fe990249423982981d8
ErikkLocatelli <erikklocatelli@gmail.com>
2026-07-26 19:48:16 -0300

    chore: first commit adding initial setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+import { Button } from './components/ui/button'
+import { Rocket, Sparkles } from "lucide-react"
+
+function App() {
+
+  return (
+    <div>
+      <h1>Teste</h1>
+      <Button>Teste</Button>
+      <Sparkles/>
+      <Button><Rocket /></Button>
+    </div>
+  )
+}
+
+export default App
